4b0fb4e09a90b6f21921ccb929ccdab609519245
Alexandre Halas <alexandrehalas@gmail.com>
2023-09-07 11:29:22 -0300

    Event Propagation - Bubbling and Capturing

---
+++ theory-04-event-propagation-bubbling-and-capturing.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// EVENT PROPAGATION - BUBBLING AND CAPTURING
+
+const randomInt = function random(min, max) {
+  return Math.floor(min + Math.random() * (max - min));
+};
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // not recommended stop propagation of events, but its possible
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+});
