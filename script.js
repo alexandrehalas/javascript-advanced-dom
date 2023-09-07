990846b1d7976f57416ad08fa4ba456d87aa6442
Alexandre Halas <alexandrehalas@gmail.com>
2023-09-07 12:53:45 -0300

    Event Delegation: Implementing Page Navigation

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 'use strict';
 
-// MODAL WINDOW
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+const navLinks = document.querySelector('.nav__links');
+
+// MODAL WINDOW
 
 const openModal = function (e) {
   e.preventDefault();
@@ -31,9 +36,19 @@ document.addEventListener('keydown', function (e) {
 
 // BUTTON SCROLL TO
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
 btnScrollTo.addEventListener('click', () =>
   section1.scrollIntoView({ behavior: 'smooth' })
 );
+
+// PAGE NAVIGATION
+
+const navigateSmooth = function (e) {
+  e.preventDefault();
+  // matching strategy
+  if (!e.target.classList.contains('nav__link')) return;
+  const id = e.target.getAttribute('href');
+  document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+};
+
+// Attached same event for multiples elements
+navLinks.addEventListener('click', navigateSmooth);
