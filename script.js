3e06f4d45f192a329623e0621eaeec32b71305d8
Alexandre Halas <alexandrehalas@gmail.com>
2023-09-13 18:10:08 -0300

    Lifecycle DOM events

--- script.js
+++ script.js
@@ -263,3 +263,38 @@ document.addEventListener('click', function (e) {
     moveToSlide(slide);
   }
 });
+
+// LIFECYCLE DOM EVENTS
+
+/**
+ * The DOMContentLoaded event fires when the HTML document has been completely parsed,
+ * and all deferred scripts (<script defer src="…"> and <script type="module">) have downloaded and executed.
+ * It doesn't wait for other things like images, subframes, and async scripts to finish loading.
+ */
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree build!');
+});
+
+/**
+ *  The load event is fired when the whole page has loaded,
+ *  including all dependent resources such as stylesheets, scripts, iframes, and images.
+ *  This is in contrast to DOMContentLoaded, which is fired as soon as the page DOM has been loaded,
+ *  without waiting for resources to finish loading.
+ *
+ *  This event is not cancelable and does not bubble.
+ */
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded!');
+});
+
+/**
+ * The beforeunload event is fired when the window, the document and its resources are about to be unloaded.
+ * The document is still visible and the event is still cancelable at this point.
+ *
+ * This event enables a web page to trigger a confirmation dialog asking the user if they really want to leave the page.
+ * If the user confirms, the browser navigates to the new page, otherwise it cancels the navigation.
+ */
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  e.returnValue = '';
+});
